1bbd58f368f89504bf48e1212e4383a67cfd7a73
Cerem Cem ASLAN <cem@aktos.io>
2018-08-26 00:36:25 +0300

    added help, implemented dry-run, added LCX container backups

--- take-snapshot.sh
+++ take-snapshot.sh
@@ -15,7 +15,64 @@ safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_S
 safe_source $_dir/smith-sync/lib/all.sh
 safe_source $_dir/config.sh
 
-[[ $(whoami) = "root" ]] || { sudo $0 $*; exit 0; }
+# show help
+show_help(){
+    cat <<HELP
+
+    $(basename $0) [options]
+
+    Options:
+
+        --dry-run       : Dry run, don't touch anything actually
+
+HELP
+    exit
+}
+
+check_dry_run(){
+    if [[ $dry_run = false ]]; then
+        "$@"
+    else
+        echo "DRY RUN: $@"
+    fi
+}
+
+# Parse command line arguments
+# ---------------------------
+# Initialize parameters
+dry_run=false
+new_hostname=
+root_dir=
+# ---------------------------
+args=("$@")
+_count=1
+while :; do
+    key="${1:-}"
+    case $key in
+        -h|-\?|--help)
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        # --------------------------------------------------------
+        --dry-run) shift
+            dry_run=true
+            ;;
+        # --------------------------------------------------------
+        --*)
+            echo
+            echo "Unknown option: $1"
+            show_help
+            exit 1
+            ;;
+        *)  # generate the positional arguments: $_arg1, $_arg2, ...
+            [[ ! -z ${1:-} ]] && declare _arg$((_count++))="$1" && shift
+    esac
+    [[ -z ${1:-} ]] && break
+done; set -- "${args[@]}"
+# use $_arg1 in place of $1, $_arg2 in place of $2 and so on, "$@" is intact
+
+# All checks are done, run as root.
+[[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
 
 start_timer
 
@@ -25,20 +82,30 @@ if is_btrfs_subvolume $ROLLBACK_SNAPSHOT; then
     # to delete a mounted snapshot
     require_not_mounted $ROLLBACK_SNAPSHOT
     echo "Removing current rollback snapshot ($ROLLBACK_SNAPSHOT)"
-    btrfs sub delete $ROLLBACK_SNAPSHOT
+    check_dry_run btrfs sub delete $ROLLBACK_SNAPSHOT
 else
     echo_yellow "No rollback snapshot found."
 fi
 
 echo_green "Snapshotting current rootfs as rollback ($ROLLBACK_SNAPSHOT)"
-btrfs sub snap / $ROLLBACK_SNAPSHOT
+check_dry_run btrfs sub snap / $ROLLBACK_SNAPSHOT
 show_timer "Current rootfs is now in rollback location."
 
 echo_green "Backing up /boot partition"
-rsync -avrP /boot/ /boot.backup/
+check_dry_run rsync -avrP /boot/ /boot.backup/
 
 POSTFIX=$(get_timestamp)
 echo_green "Taking Snapshots with $POSTFIX postfix..."
-take_snapshot "$ROOTFS/$ROOTFS_LIVE" "$ROOTFS_SNAP/$ROOTFS_LIVE/$ROOTFS_LIVE.$POSTFIX"
-take_snapshot "$ROOTFS/$HEYBE_LIVE" "$ROOTFS_SNAP/$HEYBE_LIVE/$HEYBE_LIVE.$POSTFIX"
+check_dry_run take_snapshot "$ROOTFS/$ROOTFS_LIVE" "$ROOTFS_SNAP/$ROOTFS_LIVE/$ROOTFS_LIVE.$POSTFIX"
+check_dry_run take_snapshot "$ROOTFS/$HEYBE_LIVE" "$ROOTFS_SNAP/$HEYBE_LIVE/$HEYBE_LIVE.$POSTFIX"
+
+for vm in /var/lib/lxc/*; do
+    [[ -d "$vm" ]] || continue
+    vm_name=$(basename $vm)
+    echo "...Found vm: $vm_name"
+    dst="$ROOTFS/snapshots/$vm_name"
+    check_dry_run mkdir -p $dst
+    check_dry_run take_snapshot $vm/rootfs $dst/$vm_name.$POSTFIX
+done
+
 show_timer "All done."

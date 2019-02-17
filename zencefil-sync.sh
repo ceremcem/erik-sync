81d6b685757d47492ff005bb92b6d34710ace630
Cerem Cem ASLAN <cem@aktos.io>
2019-02-17 21:13:47 +0300

    added remote backup procedures

--- zencefil-sync.sh
+++ zencefil-sync.sh
@@ -9,34 +9,56 @@ safe_source $_dir/smith-sync/lib/all.sh
 # All checks are done, run as root.
 [[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
 
-machine="erik"
-for b in rootfs cca-heybe; do
-    src=$(get-latest-folder "$ROOTFS/snapshots/$machine/$b")
-    src_name="$(basename $src)"
+backup_machine_from_erik(){
+    #local machine="fc2"
+    local machine="$1"
+    local src=$(get-latest-folder "$ROOTFS/snapshots/$machine")
+    local src_name="$(basename $src)"
     echo_green "Backing up $src_name"
-    dest="$zencefil_mnt/sync/$machine/$b"
-    snap_name="$zencefil_mnt/snapshots/$machine/$b/$src_name"
+    local dest="$zencefil_mnt/sync/$machine"
+    local snap_name="$zencefil_mnt/snapshots/$machine/$src_name"
     if [[ -e "$snap_name" ]]; then
         echo_yellow "Skipping $src_name because it's already snapshotted"
-        continue
+    else
+        [[ -d "$dest" ]] || btrfs sub create "$dest"
+        $_sdir/smith-sync/rsync.sh -u "$src/" "$dest/"
+        [[ $? -eq 0 ]] && mkdir -p $(dirname "$snap_name"); btrfs sub snap -r "$dest" "$snap_name"
     fi
-    $_sdir/smith-sync/rsync.sh -u "$src/" "$dest/"
-    [[ $? -eq 0 ]] && btrfs sub snap -r "$dest" "$snap_name"
-done
+}
+
+backup_machine_from_erik "erik/rootfs"
+backup_machine_from_erik "erik/cca-heybe"
+backup_machine_from_erik "fc2"
 
 
-# TODO: Remove this duplicate code
-machine="fc2"
-src=$(get-latest-folder "$ROOTFS/snapshots/$machine")
-src_name="$(basename $src)"
-echo_green "Backing up $src_name"
-dest="$zencefil_mnt/sync/$machine"
-snap_name="$zencefil_mnt/snapshots/$machine/$src_name"
-if [[ -e "$snap_name" ]]; then
-    echo_yellow "Skipping $src_name because it's already snapshotted"
-else
-    [[ -d "$dest" ]] || btrfs sub create "$dest"
-    $_sdir/smith-sync/rsync.sh -u "$src/" "$dest/"
-    mkdir -p $(dirname "$snap_name")
-    [[ $? -eq 0 ]] && btrfs sub snap -r "$dest" "$snap_name"
-fi
+backup_from_aea3(){
+    local machine="$1"
+    local ssh_conn="aea@192.168.1.10"
+    local ssh_settings="-p 2288 -i /home/ceremcem/.ssh/id_rsa"
+    local remote_path="/mnt/peynir/snapshots/$machine"
+    local glob_pattern="$(ssh $ssh_conn $ssh_settings ls $remote_path | sort)"
+    local newest=
+    for file in $glob_pattern ; do
+        newest=$file
+    done
+    local src="$remote_path/$newest"
+    local src_name="$(basename $src)"
+    echo_green "Backing up REMOTE:$src"
+    local dest="$zencefil_mnt/sync/$machine"
+    local snap_name="$zencefil_mnt/snapshots/$machine/$src_name"
+    if [[ -e "$snap_name" ]]; then
+        echo_yellow "Skipping $src_name because it's already snapshotted"
+    else
+        [[ -d "$dest" ]] || { echo_red "$dest does not exist."; return 3; }
+        $_sdir/smith-sync/rsync.sh -u --ssh="$ssh_settings" "$ssh_conn:$src/" "$dest/"
+        [[ $? -eq 0 ]] && mkdir -p $(dirname "$snap_name"); btrfs sub snap -r "$dest" "$snap_name"
+    fi
+}
+
+backup_from_aea3 "aea3"
+backup_from_aea3 "aktos1"
+backup_from_aea3 "aktos-cloud"
+backup_from_aea3 "aktos-couch"
+backup_from_aea3 "aktos-couch2"
+backup_from_aea3 "aktos-git"
+backup_from_aea3 "node-occ"

4f0db4518b261a32248d48d3b22e56e509f0bb06
Cerem Cem ASLAN <cem@aktos.io>
2021-01-05 04:04:07 +0300

    fixed (workaround for) zencefil-auto.sh --poll

--- zencefil-auto.sh
+++ zencefil-auto.sh
@@ -2,41 +2,14 @@
 _sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 [[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
 
-disk_exists(){
-	if [[ -z $(udevadm info -n /dev/sd* | grep ID_SERIAL=WD_Elements_10A8_575833314536335946303730-0 | uniq) ]]; then
-		return 1
-	else
-		return 0
-	fi
-}
-
-if [[ "${1:-}" == "--poll" ]]; then
-    echo "Polling: enabled."
-    poll=true
-else
-    poll=false
-fi
-
 cd $_sdir
-processed=false
-while sleep 1; do
-    if disk_exists; then
-        if [[ "$processed" = false ]]; then
-            notify-send "Backing up to zencefil."
-            t0=$EPOCHSECONDS
-            ./take-snapshot.sh
-            ./zencefil-attach.sh
-            time ./zencefil-backup.sh
-            ./zencefil-assemble-bootable.sh --refresh --full
-            ./zencefil-detach.sh
-            t1=$EPOCHSECONDS
-            notify-send -u critical "Backup of zencefil has ended." \
-                "Took `date -d@$(($t1 - $t0)) -u +%H:%M:%S` seconds. Zencefil can be unplugged safely."
-	    processed=true
-            [[ "$poll" = false ]] && break
-        fi
-    else
-        processed=false
-    fi
-done
-echo "Exiting."
+t0=$EPOCHSECONDS
+./zencefil-attach.sh
+notify-send "Backing up to zencefil."
+./take-snapshot.sh
+time ./zencefil-backup.sh
+./zencefil-assemble-bootable.sh --refresh --full
+./zencefil-detach.sh
+t1=$EPOCHSECONDS
+notify-send -u critical "Backup of zencefil has ended." \
+    "Took `date -d@$(($t1 - $t0)) -u +%H:%M:%S` seconds. Zencefil can be unplugged safely."
---
+++ zencefil-poll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Usage:
+#
+#   $ sudo -s
+#   # source $(basename $0)
+#
+# See also: https://unix.stackexchange.com/questions/627262/chroot-fails-to-be-executed-more-than-once-in-a-while-loop
+
+#_sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#[[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
+#cd $_sdir
+
+source zencefil-config.sh
+echo "Started monitoring zencefil."
+while sleep 1; do 
+    while sleep 1; do test -b /dev/disk/by-id/$wwn || break; done;
+    echo "Waiting for zencefil to attach..."
+    while sleep 1; do test -b /dev/disk/by-id/$wwn && break; done;
+    ./zencefil-auto.sh
+done

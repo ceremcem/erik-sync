42ee3d9e7520360bbc8d5834aa32c1f2458f30e6
Cerem Cem ASLAN <cem@aktos.io>
2021-01-07 08:36:56 +0300

    update configs

--- targets/masa/config.sh
+++ targets/masa/config.sh
@@ -4,8 +4,7 @@ lvm_name="masa"
 # use ./get-disk-info.sh to identify the UUID's:
 boot_part='UUID=7f7b9b8e-a773-4d4e-b448-ee194fd58a0f'
 crypt_part='UUID=d8ede8f6-a295-401a-93d8-8f5e3d3f3f2e'
-
-crypt_key="/home/ceremcem/.ssh/luks-keys/masa-key-1"
+crypt_key="$(cat ./keypath)"
 
 # you probably won't need to change those:
 crypt_dev_name=${lvm_name}_crypt
--- targets/zencefil/config.sh
+++ targets/zencefil/config.sh
@@ -4,7 +4,7 @@ lvm_name="zencefil"
 # use ./get-disk-info.sh to identify the UUID's:
 boot_part='UUID=fe2cfe8c-28d3-455c-961e-b586cf763367'
 crypt_part='UUID=32d6e3b6-1e75-4d40-86c2-5a8853996e73'
-crypt_key="/home/ceremcem/.ssh/luks-keys/zencefil-key-1"
+crypt_key="$(cat ./keypath)"
 
 # you probably won't need to change those:
 crypt_dev_name=${lvm_name}_crypt

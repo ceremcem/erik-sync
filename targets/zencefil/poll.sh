a0b8bda22c7869e122b362884a4674edc845a1b1
Cerem Cem ASLAN <cem@aktos.io>
2021-01-12 00:50:54 +0300

    improved zencefil/poll.sh invocation, improvement for #2

--- targets/zencefil/poll.sh
+++ targets/zencefil/poll.sh
@@ -5,17 +5,16 @@
 #   $ sudo -s
 #   # source $(basename $0)
 #
-# See also: https://unix.stackexchange.com/questions/627262/chroot-fails-to-be-executed-more-than-once-in-a-while-loop
+#   Known Bug: ceremcem/erik-sync#2
 
-#_sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#[[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
-#cd $_sdir
+_sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+[[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
+cd $_sdir
 
-source config.sh
-echo "Started monitoring zencefil."
-while sleep 1; do 
-    while sleep 1; do test -b /dev/disk/by-id/$wwn || break; done;
-    echo "Waiting for zencefil to attach..."
-    while sleep 1; do test -b /dev/disk/by-id/$wwn && break; done;
-    ./auto.sh
-done
+source ./config.sh
+dev="/dev/disk/by-id/$wwn"
+echo "Monitoring zencefil: $dev"
+while sleep 1; do test -b "$dev" || break; done;
+echo "Waiting for zencefil to attach..."
+while sleep 1; do test -b "$dev" && break; done;
+./auto.sh

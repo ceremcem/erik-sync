0c6b7cae1e24e31dc7b59defa37c243089ac5271
Cerem Cem ASLAN <cem@aktos.io>
2021-01-21 02:35:34 +0300

    added support to restore from a known snapshot

--- targets/zencefil/assemble-bootable.sh
+++ targets/zencefil/assemble-bootable.sh
@@ -2,6 +2,8 @@
 _sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -eu
 
+# pass "--date 20201227T0009" if you want to restore from an old
+# snapshot
 cd $_sdir
 sudo ../../smith-sync/assemble-bootable-system.sh \
     -c ./config.sh \

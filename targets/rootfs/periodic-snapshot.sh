9e2878f14c6ae72f3c9326f84a88cfd5a0d4f711
Cerem Cem ASLAN <cem@aktos.io>
2021-02-22 04:07:10 +0300

    system snapshot frequency is increased by x4. added error guard

--- targets/rootfs/periodic-snapshot.sh
+++ targets/rootfs/periodic-snapshot.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 _sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-period="12h"
+period="3h"
 notify-send "Started periodic snapshotting" "Snapshots will be taken every $period"
 echo "Perodic snapshotting started."
-while sleep $period; do 
-    $_sdir/take-snapshot.sh 
-    notify-send -u critical "take-backup.sh ended." "`date`"
+while :; do
+    if $_sdir/take-snapshot.sh; then
+        echo "`date -u`: take-backup.sh finished." 
+    else
+        notify-send -u critical "Error in take-backup.sh" \
+            "Something went wrong, please check the console."
+    fi
+    sleep $period
 done

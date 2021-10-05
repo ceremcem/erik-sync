fd26500010e294c3eaf0b4843f82320bbefac69b
Cerem Cem ASLAN <cem@aktos.io>
2021-10-05 10:00:30 +0300

    fixed forgotten --delete switch

--- targets/rootfs/periodic-snapshot.sh
+++ targets/rootfs/periodic-snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 _sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-period="3h"
+period="6h"
 notify-send "Started periodic snapshotting" "Snapshots will be taken every $period"
 echo "Perodic snapshotting started."
 while :; do

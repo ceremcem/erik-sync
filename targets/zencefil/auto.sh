b9b0e7951106113f097be95cc595359fd1664bb0
Cerem Cem ASLAN <cem@aktos.io>
2021-10-08 21:01:42 +0300

    improved UI

--- targets/zencefil/auto.sh
+++ targets/zencefil/auto.sh
@@ -35,10 +35,10 @@ duration=`date -d@$(($t1 - $t0)) -u +%H:%M:%S`
 notify-send -u critical "$hd backup completed" "Backup completed in ${duration}."
 
 zenity --info \
-    --text="Backup of $hd completed." \
-    --ok-label="Detach Now" \
+    --text="Backup of $hd completed.\n\nLeave $hd as much as possible to let btrfs-cleaner to free the empty space and btrfs scrub to perform. " \
+    --ok-label="Unmount Now" \
     --width=200
 
 ./detach.sh
-notify-send -u critical "$hd is detached."
+notify-send -u critical "$hd is unmounted."
 

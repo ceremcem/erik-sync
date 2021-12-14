0e72000d7e89d6849c10c67cd68f5b41db9414fd
Cerem Cem ASLAN <cem@aktos.io>
2021-12-14 14:24:24 +0300

    prevent taking snapshots while dpkg is running

--- targets/rootfs/periodic-snapshot.sh
+++ targets/rootfs/periodic-snapshot.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 _sdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+[[ $(whoami) = "root" ]] || { sudo "$0" "$@"; exit 0; }
+
 period="6h"
+
+# Prevent taking snapshots while dpkg is performing:
+# https://unix.stackexchange.com/q/681324/65781
+lockfile="/var/lib/dpkg/lock"
+
 notify-send "Started periodic snapshotting" "Snapshots will be taken every $period"
 echo "Perodic snapshotting started."
 while :; do
+    if (( $(lsof -t "$lockfile" | wc -w) > 0 )) ; then
+        echo "dpkg is running. Will try again in 10m" >&2
+        sleep 10m
+        continue
+    fi
+
     if $_sdir/take-snapshot.sh; then
         echo "`date -u`: take-backup.sh finished." 
     else

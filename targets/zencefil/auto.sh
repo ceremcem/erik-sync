158ee3c71c01e6b0ab04693391369fb13d8a84b0
Cerem Cem ASLAN <cem@aktos.io>
2022-03-31 04:14:39 +0300

    external disk backup waits for DPKG to finish and gets cca-suspend deferred

--- targets/zencefil/auto.sh
+++ targets/zencefil/auto.sh
@@ -7,6 +7,21 @@ do_detach(){
     notify-send -u critical "$hd is unmounted."
 }
 
+suspend_lock_file=/tmp/cca-suspend.lock.zencefil
+disable_cca_suspend(){
+    msg="* INFO: Disabling cca-suspend."
+    echo "$msg"; notify-send -u critical "$msg"
+    touch $suspend_lock_file
+}
+
+enable_cca_suspend(){
+    msg="* INFO: Enabling cca-suspend."
+    echo "$msg"; notify-send -u critical "$msg"
+    [[ -f $suspend_lock_file ]] && rm $suspend_lock_file
+}
+
+trap 'enable_cca_suspend' EXIT
+
 # Print generated config
 while read key value; do
     case $key in
@@ -22,6 +37,8 @@ target_snapshots="$target"
 echo "source: $source_snapshots"
 echo "target: $target_snapshots"
 
+disable_cca_suspend
+
 no_new=false
 [[ ${1:-} == "--no-new" ]] && no_new=true
 
@@ -29,13 +46,37 @@ hd="zencefil"
 cd $_sdir
 t0=$EPOCHSECONDS
 
+MARK_SNAPSHOTS="../../smith-sync/mark-snapshots.sh --suffix .ZENCEFIL"
+
+if [[ $no_new == false ]]; then
+    # do not take a snapshot while APT is running
+    notified=false
+    while :; do
+        if (( $(lsof -t "/var/lib/dpkg/lock" | wc -w) > 0 )) ; then
+            if ! $notified; then
+                notify-send -u critical "Waiting for DPKG to finish" "Dpkg is running. Will be re-checked in every 1m"
+                >&2 echo "$(date): Waiting for DPKG to finish. Dpkg is running. Will retry in every 1m"
+                notified=true
+            fi
+            sleep 1m
+            continue
+        else
+            break
+        fi
+    done
+    if $notified; then
+        notify-send -u critical "DPKG has finished" "Starting backup process"
+        >&2 echo "$(date): DPKG has finished. Starting backup process"
+    fi
+    ../rootfs/take-snapshot.sh
+else
+    echo "Skipping taking a new snapshot."
+fi
+
 ./detach.sh
 ./attach.sh
 notify-send "Backing up to $hd."
 
-MARK_SNAPSHOTS="../../smith-sync/mark-snapshots.sh --suffix .ZENCEFIL"
-
-[[ $no_new == false ]] && ../rootfs/take-snapshot.sh || echo "Skipping taking a new snapshot."
 $MARK_SNAPSHOTS "$source_snapshots" --unfreeze --fix-received "$target_snapshots"
 if ! time ./backup.sh; then
     notify-send -u critical "ERROR: $hd backup" "Something went wrong. Check console."
